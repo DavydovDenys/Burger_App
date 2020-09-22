d0255e9bb8e1e8cd0c51df7f31bee6fd112e3f8e
DavydovDenys <denvanrain@gmail.com>
2020-09-22 14:30:07 +0300

    if ingredients are not added, render some text

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,18 @@ import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients).map(
+  let transformedIngredients = Object.keys(props.ingredients).map(
     (igKey) => {
       // debugger;
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredients key={igKey + i} type={igKey}/>;
       });
-    });
+    }).reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   console.log(transformedIngredients);
 
 
